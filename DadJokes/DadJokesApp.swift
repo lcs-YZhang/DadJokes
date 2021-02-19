f5dac7c76eae7026ae514890f641c885d407ec9c
Steven Zhang <YZhang@lcs.on.ca>
2021-02-19 11:12:55 -0500

    Initial Commit

---
+++ DadJokes/DadJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by Steven Zhang on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
