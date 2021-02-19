c879087a98fc32b58c7e896865255920655cd8e1
Steven Zhang <YZhang@lcs.on.ca>
2021-02-19 11:25:08 -0500

    Using the .onAppear modifier to make something happen at app startup

--- DadJokes/ContentView.swift
+++ DadJokes/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var someText = "Hello World"
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(someText)
             .padding()
-    }
+            .onAppear() {
+        someText = "Right back at you"
+      }
+   }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ DadJokes/DadJoke.swift
@@ -0,0 +1,15 @@
+//
+//  DadJoke.swift
+//  DadJokes
+//
+//  Created by Steven Zhang on 2021-02-19.
+//
+
+import Foundation
+
+struct DadJoke: Decodable {
+    let id: String
+    let joke: String
+    let status: Int
+    
+}
